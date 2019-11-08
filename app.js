c9a2df9ed8f1682f9fe04c05f197cbe12179f0a2
András Jáky <jakyandras@gmail.com>
2019-11-08 12:02:59 +0100

    added other solution for upvote/downvote

--- app.js
+++ app.js
@@ -63,12 +63,12 @@ app.get('/posts', function(req, res) {
 app.post('/posts', urlencodedParser, function(req, res) {
     connection.query(`INSERT INTO posts (title, url, timestamp) VALUES ('${req.body.title}', '${req.body.url}', NOW());`, function(err, result) {
         if (err) {
-            res.status(500).send('Database error');
+            res.status(500).send('Problem with creating post');
             return;
         }
         connection.query('SELECT * FROM posts WHERE id=(SELECT max(id) FROM posts);', function(err, result) {
             if (err) {
-                res.status(500).send('Database error');
+                res.status(500).send('Problem with displaying post');
                 return;
             }
             res.status(200);
@@ -82,18 +82,18 @@ app.put('/posts/:id/upvote', function(req, res) {
     connection.query(`SELECT score FROM posts WHERE id = '${req.params.id}';`, function (err, result) {
         if (err) {
             console.log(err.toString());
-            res.status(500).send('Database error 1');
+            res.status(500).send('There is no such post');
             return;
         }
         connection.query(`UPDATE posts SET score = score + 1 WHERE id = '${req.params.id}';`, function(err, result) {
             if (err) {
                 console.log(err.toString());
-                res.status(500).send('Database error 2');
+                res.status(500).send('Problem with updating');
                 return;
             }
             connection.query(`SELECT * FROM posts WHERE id = '${req.params.id}';`, function(err, result) {
                 if (err) {
-                    res.status(500).send('Database error 3');
+                    res.status(500).send('Problem with displaying the modified post');
                     return;
                 }
                 res.status(200);
@@ -103,6 +103,34 @@ app.put('/posts/:id/upvote', function(req, res) {
         });
     });
 });
+
+// Viktorr's async method:
+// app.put('/posts/:id/upvote', async function(req, response) {
+//     let result = await getScoreById(req.params.id);
+//     if (result.length > 0) {
+//         await incrementScoreByOneById(req.params.id);
+//     }
+//     let newResult = await getScoreById(req.params.id);
+//     response.setHeader("Content-type", "application/json");
+//     response.send(newResult[0]);
+// });
+
+// async function incrementScoreByOneById(id) {
+//     return new Promise((resolve, reject) => {
+//         connection.query(`UPDATE posts SET score = score + 1 WHERE id = '${id}';`, (err, result) => {
+//             resolve(result);
+//         });
+//     });
+// }
+
+// async function getScoreById(id){
+//     // console.log(connection);
+//     return new Promise((resolve, reject) => {
+//         connection.query(`SELECT score FROM posts WHERE id = '${id}';`, (err, result) => {
+//             resolve(result);
+//         });
+//     });
+// }
 
 app.put('/posts/:id/downvote', function(req, res) {
     connection.query(`SELECT score FROM posts WHERE id = '${req.params.id}';`, function (err, result) {
@@ -129,6 +157,16 @@ app.put('/posts/:id/downvote', function(req, res) {
         });
     });
 });
+
+// absolute barebone solution with no error handling
+// app.put('/posts/:id/downvote', function(req, res) {
+//     connection.query(`UPDATE posts SET score = score - 1 WHERE id = '${req.params.id}';`);
+//     connection.query(`SELECT * FROM posts WHERE id = '${req.params.id}';`, function(err, result) {
+//         res.status(200);
+//         res.setHeader("Content-type", "application/json");
+//         res.send(result[0]);
+//     });
+// });
 
 app.listen(PORT, () => {
     console.log(`The server is up and running on ${PORT}`);
