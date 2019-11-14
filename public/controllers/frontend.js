0077f81e66b0d50291df030dde319481e8859252
András Jáky <jakyandras@gmail.com>
2019-11-14 14:59:48 +0100

    started to work on loadPosts function

--- public/controllers/frontend.js
+++ public/controllers/frontend.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const config = require('../..config');
+const port = config.app.port;
+
+// function loadRedditFront () {
+
+// }
+
+function loadPosts () {
+    fetch (`http://localhost:${port}/posts/`)
+    .then(response => response.json())
+    // .then(myJson => joke.textContent = myJson.value.joke)
+    // .catch((reason) => {
+    //     console.log(reason);
+    //     console.log('Some error happened');
+    // });
+}
+
+// window.onload = loadRedditFront;
