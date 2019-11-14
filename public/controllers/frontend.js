1f4a52c3092b650c6368b03054000f3d05ee1533
András Jáky <jakyandras@gmail.com>
2019-11-14 21:59:33 +0100

    created function to read the posts

--- public/controllers/frontend.js
+++ public/controllers/frontend.js
@@ -1,20 +1,30 @@
 'use strict';
 
-const config = require('../..config');
-const port = config.app.port;
+// const config = require('../../config');
+// const port = config.app.port;
 
-// function loadRedditFront () {
-
+// function loadPosts () {
+//     // fetch (`http://localhost:${port}/posts/`)
+//     fetch ('http://localhost:3000/posts/')
+//     .then(response => response.json())
+//     .then(myJson => {
+//         console.log(myJson);
+//     })
+//     .catch((reason) => {
+//         console.log(reason);
+//         console.log('Some error happened');
+//     });
 // }
 
-function loadPosts () {
-    fetch (`http://localhost:${port}/posts/`)
-    .then(response => response.json())
-    // .then(myJson => joke.textContent = myJson.value.joke)
-    // .catch((reason) => {
-    //     console.log(reason);
-    //     console.log('Some error happened');
-    // });
+async function loadPosts () {
+    let responseJson = {};
+    try {
+        let response = await fetch('http://localhost:3000/posts/');
+        responseJson = await response.json();
+    } catch (reason) {
+        console.log(reason);
+    }
+    console.log(responseJson);
 }
 
-// window.onload = loadRedditFront;
+window.onload = loadPosts;
