6e171f784550d403e08a0a4c7d77f18f032e0a38
András Jáky <jakyandras@gmail.com>
2019-11-15 21:41:15 +0100

    started to work on submit

--- public/controllers/frontend-submit.js
+++ public/controllers/frontend-submit.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const addForm = document.forms;
+
+addForm.addEventListener = ('submit', function (e) {
+    e.preventDefault();    // mivel a default behavior hogy ilyenkor újratölti az oldalt, de mi ezt most nem akarjuk
+    // const value = addForm.querySelector('input[type="text"]').value  // az érték amit bevittek a mezőbe
+    console.log('clicked');
+});
