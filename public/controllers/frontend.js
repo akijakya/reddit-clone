3fa625559b72e8465aec1335c8c0b3f34921df39
András Jáky <jakyandras@gmail.com>
2019-11-15 15:51:03 +0100

    implemented upvote/downvote system - and it only works for once for each of the articles within one session!

--- public/controllers/frontend.js
+++ public/controllers/frontend.js
@@ -1,26 +1,15 @@
 'use strict';
 
-// const config = require('../../config');
-// const port = config.app.port;
+let upVoted = false;
+let downVoted = false;
 
-// function loadPosts () {
-//     // fetch (`http://localhost:${port}/posts/`)
-//     fetch ('http://localhost:3000/posts/')
-//     .then(response => response.json())
-//     .then(myJson => {
-//         console.log(myJson);
-//     })
-//     .catch((reason) => {
-//         console.log(reason);
-//         console.log('Some error happened');
-//     });
-// }
+let url = 'http://localhost:3000';
 
 async function loadPosts () {
     let responseJson = {};
 
     try {
-        let response = await fetch('http://localhost:3000/posts/');
+        let response = await fetch(`${url}/posts/`);
         responseJson = await response.json();
     } catch (reason) {
         console.log(reason);
@@ -29,11 +18,74 @@ async function loadPosts () {
     responseJson.posts.forEach(e => createArticle(e));
 }
 
+async function upVote (id) {
+    let responseJson = {};
+    let article = document.getElementById(`${id}`);
+    let arrowUp = article.getElementsByTagName('img')[0];
+    let arrowDown = article.getElementsByTagName('img')[1];
+
+    if (arrowUp.src === `${url}/assets/arrows/upvote.png`) {
+        try {
+            let response = await fetch(`${url}/posts/${id}/upvote`, {
+                method: "PUT"
+            });
+            responseJson = await response.json();
+            arrowUp.src = "../assets/arrows/upvoted.png"
+
+            if (arrowDown.src === `${url}/assets/arrows/downvoted.png`) {
+                let response = await fetch(`${url}/posts/${id}/upvote`, {
+                method: "PUT"
+                });
+                responseJson = await response.json();
+                arrowDown.src = "../assets/arrows/downvote.png"
+            }
+        } catch (reason) {
+            console.log(reason);
+        }
+
+        // changing the displayed number
+        let scoreNumber = article.getElementsByTagName('h1')[0];
+        scoreNumber.textContent = responseJson.score;
+    }
+}
+
+async function downVote (id) {
+    let responseJson = {};
+    let article = document.getElementById(`${id}`);
+    let arrowUp = article.getElementsByTagName('img')[0];
+    let arrowDown = article.getElementsByTagName('img')[1];
+
+    if (arrowDown.src === `${url}/assets/arrows/downvote.png`) {
+        try {
+            let response = await fetch(`${url}/posts/${id}/downvote`, {
+                method: "PUT"
+            });
+            responseJson = await response.json();
+            arrowDown.src = "../assets/arrows/downvoted.png"
+
+            if (arrowUp.src === `${url}/assets/arrows/upvoted.png`) {
+                let response = await fetch(`${url}/posts/${id}/downvote`, {
+                method: "PUT"
+                });
+                responseJson = await response.json();
+                arrowUp.src = "../assets/arrows/upvote.png"
+            }
+        } catch (reason) {
+            console.log(reason);
+        }
+
+        // changing the displayed number
+        let scoreNumber = article.getElementsByTagName('h1')[0];
+        scoreNumber.textContent = responseJson.score;
+    }
+}
+
 function createArticle (post) {
     // building up vote-section
     let upvote = document.createElement('img');
     upvote.className = 'upvote';
     upvote.src = "../assets/arrows/upvote.png";
+    upvote.setAttribute('onclick', `upVote(${post.id});`);
 
     let scoreCount = document.createElement('h1');
     scoreCount.textContent = post.score;
@@ -41,6 +93,7 @@ function createArticle (post) {
     let downvote = document.createElement('img', { class: 'downvote', src: "../assets/arrows/downvote.png"});
     downvote.className = 'downvote';
     downvote.src = "../assets/arrows/downvote.png";
+    downvote.setAttribute('onclick', `downVote(${post.id});`);
 
     let voteSection = document.createElement('div');
     voteSection.className = 'vote-section'
@@ -89,6 +142,7 @@ function createArticle (post) {
 
     // adding the elements to a li and then the ul element
     let newLi = document.createElement('li');
+    newLi.id = post.id;
     newLi.appendChild(voteSection);
     newLi.appendChild(articleSection);
 
@@ -108,25 +162,15 @@ function timeDifference(current, previous) {
 
     if (elapsed < msPerMinute) {
          return Math.round(elapsed/1000) + ' seconds ago';   
-    }
-
-    else if (elapsed < msPerHour) {
+    } else if (elapsed < msPerHour) {
          return Math.round(elapsed/msPerMinute) + ' minutes ago';   
-    }
-
-    else if (elapsed < msPerDay ) {
+    } else if (elapsed < msPerDay ) {
          return Math.round(elapsed/msPerHour ) + ' hours ago';   
-    }
-
-    else if (elapsed < msPerMonth) {
+    } else if (elapsed < msPerMonth) {
         return Math.round(elapsed/msPerDay) + ' days ago';   
-    }
-
-    else if (elapsed < msPerYear) {
+    } else if (elapsed < msPerYear) {
         return Math.round(elapsed/msPerMonth) + ' months ago';   
-    }
-
-    else {
+    } else {
         return Math.round(elapsed/msPerYear ) + ' years ago';   
     }
 }
