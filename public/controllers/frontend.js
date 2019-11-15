ecb83d16e32a35fe804a97e1ac890db6fbda1b7a
András Jáky <jakyandras@gmail.com>
2019-11-15 09:20:13 +0100

    added function to return elapsed time

--- public/controllers/frontend.js
+++ public/controllers/frontend.js
@@ -59,11 +59,15 @@ function createArticle (post) {
     title.appendChild(articleLink);
 
     let submitted = document.createElement('p');
+    let timeDiff = timeDifference(Date.now(), post.timestamp);
+    console.log(timeDiff);
+    submitted.textContent = 'submitted ' + timeDiff + ' by'
 
 
     let articleSection = document.createElement('div');
     articleSection.className = 'article-section';
     articleSection.appendChild(title);
+    articleSection.appendChild(submitted);
 
     //adding the elements to a li and then the ul element
     let newLi = document.createElement('li');
@@ -74,6 +78,39 @@ function createArticle (post) {
     articleList.appendChild(newLi);
 }
 
+function timeDifference(current, previous) {
 
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
 
 window.onload = loadPosts;
