3e304625584197d13228f3f79ceeaab2ba86ef7d
András Jáky <jakyandras@gmail.com>
2019-11-18 11:43:27 +0100

    started to work on submitting to the database

--- public/controllers/frontend-submit.js
+++ public/controllers/frontend-submit.js
@@ -1,9 +1,45 @@
 'use strict';
 
-const addForm = document.forms;
+const url = 'http://localhost:3000';
+const submitForm = document.forms['submit-article'];
 
-addForm.addEventListener = ('submit', function (e) {
+submitForm.addEventListener ('submit', function (e) {
     e.preventDefault();    // mivel a default behavior hogy ilyenkor újratölti az oldalt, de mi ezt most nem akarjuk
-    // const value = addForm.querySelector('input[type="text"]').value  // az érték amit bevittek a mezőbe
-    console.log('clicked');
+    const title = submitForm.querySelector('textarea[type="text"]').value ; // az érték amit bevittek a mezőbe
+    const url = submitForm.querySelector('input[type="url"]').value;
+    const data = {
+        "title": title,
+        "url": url
+    };
+    
+    console.log(data);
+
+    // fetch (`${url}/posts/`, {
+    //         method: "POST",
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json'
+    //         }
+    //     })
+    //     .then((response) => {
+    //         console.log(response);
+    //         response.json().then((responseJson) => {
+    //             console.log(responseJson);
+    //         });
+    //     });
+    try {
+        let response = fetch(`${url}/posts/`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        let responseJson = response.json();
+        console.log(responseJson);
+    } catch (reason) {
+        console.log(reason);
+    }
 });
