e46d871aece924fd181ababc61512ff18bdc84ac
akijakya <jakyandras@gmail.com>
2020-02-27 13:32:22 +0100

    dumped a new verison of the database

--- reddit.sql
+++ reddit.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+-- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: reddit
 -- ------------------------------------------------------
--- Server version	8.0.18
+-- Server version	10.3.22-MariaDB-1:10.3.22+maria~eoan
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,30 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `posts`
+-- Table structure for table `redditposts`
 --
 
-DROP TABLE IF EXISTS `posts`;
+DROP TABLE IF EXISTS `redditposts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `posts` (
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `redditposts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `url` text NOT NULL,
-  `timestamp` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `score` int(11) NOT NULL DEFAULT '0',
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `timestamp` bigint(20) NOT NULL,
+  `score` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `posts`
+-- Dumping data for table `redditposts`
 --
 
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,'Dear JavaScript','https://9gag.com','2019-10-31 12:18:27',0),(2,'Crockford','https://reddit.com','2019-10-31 12:19:05',0),(3,'undefined','undefined','2019-11-08 08:35:33',2),(4,'undefined','undefined','2019-11-04 15:19:11',0),(5,'undefined','undefined','2019-11-04 15:20:53',0),(6,'undefined','undefined','2019-11-04 15:21:13',0),(7,'undefined','undefined','2019-11-04 15:21:59',0),(8,'undefined','undefined','2019-11-04 15:22:28',0),(9,'undefined','undefined','2019-11-04 15:23:15',0),(10,'undefined','undefined','2019-11-04 15:26:04',0),(11,'undefined','undefined','2019-11-04 15:26:38',0),(12,'undefined','undefined','2019-11-04 15:26:52',0),(13,'Crockford','http://9gag.com','2019-11-04 15:30:32',0),(14,'Crockford','http://9gag.com','2019-11-04 15:30:59',0),(15,'Crockford','http://9gag.com','2019-11-04 16:14:44',0),(16,'Crockford','http://9gag.com','2019-11-04 16:14:48',0),(17,'Crockford','http://9gag.com','2019-11-05 09:47:28',0),(18,'Crockford','http://9gag.com','2019-11-08 08:35:04',0);
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+LOCK TABLES `redditposts` WRITE;
+/*!40000 ALTER TABLE `redditposts` DISABLE KEYS */;
+INSERT INTO `redditposts` VALUES (1,'proba1','http://www.reddit.com',1582806346494,0),(2,'proba3','https://9gag.com',1582806544961,0);
+/*!40000 ALTER TABLE `redditposts` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-08 10:46:42
+-- Dump completed on 2020-02-27 13:31:47
